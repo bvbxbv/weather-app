3d79214c3e2a3219b5543987868ce074f2e1b16e
Waylon Smithers <aburach@inbox.ru>
2026-02-01 21:01:16 +0700

    chore(layout): add page header
    - add lucide icons in project deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { ChevronDown, Settings, SunSnow } from 'lucide-react';
 import './styles/main.scss';
 
 function App() {
-  return <>Hello world</>;
+  return (
+    <div className="page">
+      <div className="page__root">
+        <header className="page-header">
+          <div className="page-header__item page-header__item--logo">
+            <div className="page-header__item-icon">
+              <SunSnow size={36} />
+            </div>
+            <div className="page-header__item-text">Weather now</div>
+          </div>
+
+          <div className="page-header__item page-header__item--dropdown hoverable">
+            <div className="page-header__item-icon">
+              <Settings size={16} />
+            </div>
+
+            <div className="page-header__item-text">Units</div>
+
+            <div className="page-header__item-icon">
+              <ChevronDown size={16} />
+            </div>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
 }
 
 export default App;
