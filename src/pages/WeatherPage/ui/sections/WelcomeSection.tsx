c16d218370e5b7accb38a24200721a5ce6095100
Waylon Smithers <aburach@inbox.ru>
2026-02-23 02:00:26 +0700

    feat(ux): made toggling dropdown menu with click on input

--- src/pages/WeatherPage/ui/sections/WelcomeSection.tsx
+++ src/pages/WeatherPage/ui/sections/WelcomeSection.tsx
@@ -4,6 +4,7 @@ import { useLocations } from '../../hooks/useLocations';
 import { Input } from '../components/Input/Input';
 
 export const WelcomeSection = () => {
+  const [opened, setOpened] = useState<boolean>(false);
   const [query, setQuery] = useState<string>('');
   const [apiUrl, setApiUrl] = useState<string>(
     `https://geocoding-api.open-meteo.com/v1/search?name=${query ?? ''}&count=10&language=auto&format=json`,
@@ -32,13 +33,25 @@ export const WelcomeSection = () => {
           <div className="welcome__search-wrapper">
             <SearchIcon size={20} className="welcome__search-icon" />
 
-            <Input name="city" placeholder="Search for a place..." onChange={(v) => setQuery(v)} />
+            <Input
+              name="city"
+              placeholder="Search for a place..."
+              onChange={(v) => {
+                setQuery(v);
+                setOpened(true);
+              }}
+              onClick={() => {
+                if (query.length !== 0) {
+                  setOpened(true);
+                }
+              }}
+            />
           </div>
 
           <button className="welcome__search-submit">Search</button>
         </form>
 
-        <div className="input__dropdown">
+        <div className={'input__dropdown ' + (opened ? 'visible' : 'hidden')}>
           {/* TODO: сделать сокрытие дропдауна если был клик по итему или вне дропдауна */}
           <ul className={data && data?.length > 5 ? 'scrollable' : ''}>
             {data &&
